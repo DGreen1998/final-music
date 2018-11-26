6ccb9b9e0ab57ebb4cef75273db266b18903e8b8
gd115 <1741907429@qq.com>
2018-11-26 14:02:14 +0800

    s

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -2,9 +2,9 @@
 namespace app\index\controller;
 use think\Controller;
 
-class Log extends Controller
+class Login extends Controller
 {
-	public function log()
+	public function login()
 	{
 		echo 1;
 		var_dump(input('code'));		
