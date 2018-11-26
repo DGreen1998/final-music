8ffd705e6b2301f7b82fd43d9c6fff47b0b5ff42
gd115 <1741907429@qq.com>
2018-11-26 19:55:52 +0800

    a

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -1,11 +1,15 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
+use my\Curl;
 
 class Login extends Controller
 {
 	public function login()
 	{
-		var_dump(input('code'));		
+		//使用code凭证请求微信登陆接口，获取openid和session_key
+		$url = 'https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code';
+		$curl = new Curl($url , 0);
+		var_dump($curl);
 	}
 }
